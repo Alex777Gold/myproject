ffe718a7fac47f8df0afdf39291119c9a6d57f82
Student KIt-211 <karvaholx@gmail.com>
2023-07-23 22:43:51 +0300

    work!_menu

--- script/code.js
+++ script/code.js
@@ -31,6 +31,7 @@ const toggleMenuButton = document.getElementById("menu-icon");
 const mainMenu = document.querySelector(".list");
 const logoImg = document.querySelector(".logo-img");
 const closing = document.querySelector(".mobile");
+const mobile_background = document.querySelector(".mobile_background");
 
 
 toggleMenuButton.addEventListener("click", function () {
@@ -38,9 +39,11 @@ toggleMenuButton.addEventListener("click", function () {
   closing.style.display = "block";
   logoImg.style.display = "none";
   toggleMenuButton.style.display = "none";
+  mobile_background.style.display = "block";
 });
 
 closing.addEventListener("click", function () {
   mainMenu.style.display = "none";
   toggleMenuButton.style.display = "block";
+  mobile_background.style.display = "none";
 });
