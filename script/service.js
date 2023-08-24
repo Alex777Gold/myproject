866f9e4dd703c1f784d837c919b561f2de512140
Student KIt-211 <karvaholx@gmail.com>
2023-08-24 23:29:32 +0300

    service

---
+++ script/service.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $("#addTaskBtn").click(function () {
+      var inputValue = $("#taskInput").val();
+      const settings = {
+        async: true,
+        crossDomain: true,
+        url:
+          "https://twitter241.p.rapidapi.com/user?username=" + inputValue,
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "SECRET KEY",
+          "X-RapidAPI-Host": "twitter241.p.rapidapi.com",
+        },
+      };
+
+      $.ajax(settings).done(function (response) {
+        let name = response.data.user.result.legacy.name;
+        let data_create = response.data.user.result.legacy.created_at;
+        let account_img =
+          response.data.user.result.legacy.profile_image_url_https;
+        // $("#name").text("Name: " + name);
+        // $("#data").text("Data create: " + data_create);
+
+        const taskText = $("#taskInput").val().trim();
+        if (taskText !== "") {
+          const taskItem = $("<li>").append(
+            "<span>" +
+              "Name: " +
+              name +
+              " Data create: " +
+              data_create +
+              "</span></br><img src=" +
+              account_img +
+              " alt='No image'></br><button>Delete</button>"
+          );
+          $("#taskList").append(taskItem);
+          $("#taskInput").val("");
+        }
+      });
+    });
+
+    $("#taskList").on("click", "button", function () {
+      $(this).closest("li").remove();
+    });
+
+    $("#taskList").on("click", "input[type='checkbox']", function () {
+      const taskItem = $(this).closest("li");
+      if ($(this).prop("checked")) {
+        taskItem.addClass("completed");
+      } else {
+        taskItem.removeClass("completed");
+      }
+    });
+  });
