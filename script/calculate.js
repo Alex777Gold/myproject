ef1e8e5076c1644d725df480c9080f261a413cb9
student <hanila6409@paldept.com>
2023-08-28 13:43:50 +0300

    number_array_type

--- script/calculate.js
+++ script/calculate.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-    $('#calcuklate_regresion').click(function() {
+    $('#calculate_regresion').click(function() {
         let value_x = []
         let data_value_x = $('#datax').val();
         value_x.push(data_value_x);
@@ -8,16 +8,31 @@ $(document).ready( function() {
         let data_value_y = $('#datay').val();
         value_y.push(data_value_y);
 
+        value_x = value_x.toString();
+        let value_x_array = [];
+
+        value_y = value_y.toString();
+
+        // value_x = value_x.split(",").map(value => value.codePointAt(0) - 96);
+        // for (let index = 0; index < value_x.length; index++) {
+        //     value_x_array.push(value_x[index].charCodeAt(0));
+        // }
+
+        value_x = value_x.split(",").map(value => value.codePointAt(0) - 96);
+        console.log(value_x);
+        value_y = value_y.split(",").map(value => value.codePointAt(0) - 96);
+        console.log(value_y);
+
+        
+
         let calculate = regression_line(value_x, value_y);
-        $('#test_tesult').text("Result: " + value_x + " : " + value_y);
         $('#calculate_result').text("Result: " + calculate);
-        console.log(regression_line(value_x, value_y));
 
     });
 });
 
 function regression_line(x, y) {
-    let n_length = x.length;
+    const n_length = x.length;
 
     let sum_x = 0;
     let sum_y = 0;
@@ -31,17 +46,26 @@ function regression_line(x, y) {
       sum_xy += x[index] * y[index];
     }
 
-    let a_result =
+    const a_result =
       (sum_x_squared * sum_y - sum_x * sum_xy) /
       (n_length * sum_x_squared - sum_x * sum_x);
-    let b_result =
+    const b_result =
       (n_length * sum_xy - sum_x * sum_y) /
       (n_length * sum_x_squared - sum_x * sum_x);
 
+      console.log(parseFloat(a_result.toFixed(4)) + "," + parseFloat(b_result.toFixed(4)));
+
     return [
       parseFloat(a_result.toFixed(4)),
-      parseFloat(b_result.toFixed(4))
+      parseFloat(b_result.toFixed(4)),
     ];
+    // return parseFloat(a_result.toFixed(4)) + " , " + parseFloat(b_result.toFixed(4));
   }
 
-  console.log(regression_line([1,2,3],[1,2,3]));
+  let first = [25,30,35,40,45,50];
+  let second = [78,70,65,58,48,42];
+
+  console.log(first);
+  console.log(second);
+
+  console.log(regression_line(first, second));
